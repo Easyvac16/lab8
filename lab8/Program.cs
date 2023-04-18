b141e113c178eced807dda85c396822d1abfb66a
Easyvac16 <gdima9051@gmail.com>
2023-04-18 19:03:37 +0300

    Add "cs1","cs2"."cs3"

--- lab8/Program.cs
+++ lab8/Program.cs
@@ -4,9 +4,20 @@
     {
         static void Main()
         {
-
-            cs1.task_1();
-            cs2.task_2();
+            
+            /* Console.WriteLine("Write what program you need 1 - Fraction , 2 - ComplexNumbers:");
+             int a = Convert.ToInt32(Console.ReadLine());
+             switch (a)
+             {
+                 case 1:
+                     cs1.task_1();
+                     break;
+                 case 2:
+                     cs2.task_2();
+                     break;
+             }*/
+            cs3.task_3();
+            
         }
     }
 }
---
+++ lab8/cs3.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab8
+{
+    
+    public struct Birthday
+    {
+        
+        private DateTime _birthdate;
+
+        public Birthday(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public string GetDayOfWeek()
+        {
+            return _birthdate.ToString("dddd");
+        }
+
+        public string GetDayOfWeekInYear(int year)
+        {
+            DateTime nextBirthday = _birthdate.AddYears(year - _birthdate.Year);
+            return nextBirthday.ToString("dddd");
+        }
+
+        public int GetDaysUntilBirthday()
+        {
+            DateTime today = DateTime.Today;
+            DateTime nextBirthday = _birthdate.AddYears(today.Year - _birthdate.Year);
+            if (nextBirthday < today)
+                nextBirthday = nextBirthday.AddYears(1);
+            return (nextBirthday - today).Days;
+        }
+    }
+    internal class cs3
+    {
+        public static void task_3()
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Birthday bday = new Birthday(new DateTime(2006, 1, 3));
+            Console.WriteLine(bday.GetDayOfWeek()); 
+            Console.WriteLine(bday.GetDayOfWeekInYear(2024)); 
+            Console.WriteLine(bday.GetDaysUntilBirthday()); 
+        }
+    }
+}
