63e6238d2d6e23a4489e0a173216c71590393105
Easyvac16 <gdima9051@gmail.com>
2023-04-18 19:06:35 +0300

    Little changes

--- lab8/Program.cs
+++ lab8/Program.cs
@@ -4,19 +4,19 @@
     {
         static void Main()
         {
-            
-            /* Console.WriteLine("Write what program you need 1 - Fraction , 2 - ComplexNumbers:");
-             int a = Convert.ToInt32(Console.ReadLine());
-             switch (a)
-             {
-                 case 1:
-                     cs1.task_1();
-                     break;
-                 case 2:
-                     cs2.task_2();
-                     break;
-             }*/
-            cs3.task_3();
+
+            Console.WriteLine("Write what program you need 1 - Fraction , 2 - ComplexNumbers:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            switch (a)
+            {
+                case 1:
+                    cs1.task_1();
+                    break;
+                case 2:
+                    cs2.task_2();
+                    break;
+            }
+            //cs3.task_3();
             
         }
     }
